2fe61c51647076de1bdedc9ffdfe767b760a782e
Kenta Kubo <kkk669@users.noreply.github.com>
2019-04-07 02:43:55 +0900

    Improve live view settings

--- CoreML.playgroundbook/Contents/Chapters/ObjectRecognition.playgroundchapter/Pages/image.playgroundpage/main.swift
+++ CoreML.playgroundbook/Contents/Chapters/ObjectRecognition.playgroundchapter/Pages/image.playgroundpage/main.swift
@@ -9,7 +9,11 @@ let threshold: Float = 0.3
 
 // Views
 let imageView = UIImageView(image: uiImage)
-let stackView = UIStackView(arrangedSubviews: [imageView])
+imageView.contentMode = .scaleAspectFit
+let label = UILabel()
+label.textAlignment = .center
+label.backgroundColor = #colorLiteral(red: 0.258823543787003, green: 0.756862759590149, blue: 0.968627452850342, alpha: 1.0)
+let stackView = UIStackView(arrangedSubviews: [imageView, label])
 stackView.axis = .vertical
 PlaygroundPage.current.liveView = stackView
 
@@ -20,13 +24,7 @@ let model = try VNCoreMLModel(for: try MLModel(contentsOf: compiledUrl))
 let coremlRequest = VNCoreMLRequest(model: model) { request, error in
     guard let observations = request.results as? [VNClassificationObservation] else { fatalError() }
     observations.filter { $0.confidence >= threshold }.forEach {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.backgroundColor = #colorLiteral(red: 0.258823543787003, green: 0.756862759590149, blue: 0.968627452850342, alpha: 1.0)
         label.text = "\($0.identifier): \($0.confidence)"
-        DispatchQueue.main.async {
-            stackView.addArrangedSubview(label)
-        }
     }
 }
 
