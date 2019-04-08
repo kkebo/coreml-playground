3e6a46cc4ed77a15befc260326b1b4f1737bc1c5
Kenta Kubo <kkk669@users.noreply.github.com>
2019-04-08 23:17:58 +0900

    Revert image classifier example

--- CoreML.playgroundbook/Contents/Chapters/ObjectRecognition.playgroundchapter/Pages/image.playgroundpage/main.swift
+++ CoreML.playgroundbook/Contents/Chapters/ObjectRecognition.playgroundchapter/Pages/image.playgroundpage/main.swift
@@ -10,10 +10,7 @@ let threshold: Float = 0.3
 // Views
 let imageView = UIImageView(image: uiImage)
 imageView.contentMode = .scaleAspectFit
-let label = UILabel()
-label.textAlignment = .center
-label.backgroundColor = #colorLiteral(red: 0.258823543787003, green: 0.756862759590149, blue: 0.968627452850342, alpha: 1.0)
-let stackView = UIStackView(arrangedSubviews: [imageView, label])
+let stackView = UIStackView(arrangedSubviews: [imageView])
 stackView.axis = .vertical
 PlaygroundPage.current.liveView = stackView
 
@@ -24,7 +21,13 @@ let model = try VNCoreMLModel(for: try MLModel(contentsOf: compiledUrl))
 let coremlRequest = VNCoreMLRequest(model: model) { request, error in
     guard let observations = request.results as? [VNClassificationObservation] else { fatalError() }
     observations.filter { $0.confidence >= threshold }.forEach {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = #colorLiteral(red: 0.258823543787003, green: 0.756862759590149, blue: 0.968627452850342, alpha: 1.0)
         label.text = "\($0.identifier): \($0.confidence)"
+        DispatchQueue.main.async {
+            stackView.addArrangedSubview(label)
+        }
     }
 }
 
