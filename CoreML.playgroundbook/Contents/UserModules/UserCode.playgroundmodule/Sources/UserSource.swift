9297c556e83687ca893db62ef44d543c1d56db59
Kenta Kubo <kkk669@users.noreply.github.com>
2019-06-07 00:06:12 +0900

    Add configuration option to compileModel()

--- CoreML.playgroundbook/Contents/UserModules/UserCode.playgroundmodule/Sources/UserSource.swift
+++ CoreML.playgroundbook/Contents/UserModules/UserCode.playgroundmodule/Sources/UserSource.swift
@@ -2,8 +2,12 @@ import Vision
 import CoreML
 
 public func compileModel(at url: URL) throws -> VNCoreMLModel {
+    return try compileModel(at: url, configuration: MLModelConfiguration())
+}
+
+public func compileModel(at url: URL, configuration: MLModelConfiguration) throws -> VNCoreMLModel {
     let compiledUrl = try MLModel.compileModel(at: url)
-    let mlModel = try MLModel(contentsOf: compiledUrl)
+    let mlModel = try MLModel(contentsOf: compiledUrl, configuration: configuration)
     return try VNCoreMLModel(for: mlModel)
 }
 
