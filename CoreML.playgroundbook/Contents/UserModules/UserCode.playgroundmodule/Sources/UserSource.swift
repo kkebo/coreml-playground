e65d0cd2fda41468141b3fe5de0f2a58d150fc8e
Kenta Kubo <kkk669@users.noreply.github.com>
2020-02-04 01:30:27 +0900

    Remove unnecessary code

--- CoreML.playgroundbook/Contents/UserModules/UserCode.playgroundmodule/Sources/UserSource.swift
+++ CoreML.playgroundbook/Contents/UserModules/UserCode.playgroundmodule/Sources/UserSource.swift
@@ -3,20 +3,13 @@ import CoreML
 import Accelerate
 
 public func compileModel(at url: URL) throws -> VNCoreMLModel {
-    return try compileModel(at: url, configuration: MLModelConfiguration())
+    try compileModel(at: url, configuration: MLModelConfiguration())
 }
 
 public func compileModel(at url: URL, configuration: MLModelConfiguration) throws -> VNCoreMLModel {
     let compiledUrl = try MLModel.compileModel(at: url)
     let mlModel = try MLModel(contentsOf: compiledUrl, configuration: configuration)
     return try VNCoreMLModel(for: mlModel)
-}
-
-public extension DispatchTime {
-    public func durationSec(since: DispatchTime) -> Float64 {
-        let elapsedNano = self.uptimeNanoseconds - since.uptimeNanoseconds
-        return Float64(elapsedNano) / 1_000_000_000
-    }
 }
 
 extension CGSize {
